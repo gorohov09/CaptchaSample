693de9c2173e112a117f7bc3c8f54126cade3ef3
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-05-27 22:55:25 +0300

    Контроллер для проверки капчи

--- CaptchaSample/Controllers/PortalController.cs
+++ CaptchaSample/Controllers/PortalController.cs
@@ -1,10 +1,23 @@
 ﻿using CaptchaSample.Models;
+using DNTCaptcha.Core;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
 
 namespace CaptchaSample.Controllers
 {
     public class PortalController : Controller
     {
+        private readonly IDNTCaptchaValidatorService _validatorService;
+        private readonly DNTCaptchaOptions _captchaOptions;
+
+        public PortalController(IDNTCaptchaValidatorService validatorService,
+            IOptions<DNTCaptchaOptions> options)
+        {
+            _validatorService = validatorService;
+            _captchaOptions = options == null ? throw new ArgumentNullException(nameof(options)) : options.Value;
+        }
+
         [HttpGet]
         public IActionResult Login()
         {
@@ -15,6 +28,14 @@ namespace CaptchaSample.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Login(LoginViewModel loginViewModel)
         {
+            if (ModelState.IsValid)
+            {
+                if (!_validatorService.HasRequestValidCaptchaEntry(Language.English, DisplayMode.ShowDigits))
+                {
+                    ModelState.AddModelError(_captchaOptions.CaptchaComponent.CaptchaInputName, "Please Enter Valid Captcha.");
+                    return View("Login");
+                }
+            }
             return View();
         }
     }
