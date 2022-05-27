239b7682a10915a7ede3b05aae6011b441d9c9b2
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-05-27 21:47:32 +0300

    Создание заготовки контроллера

---
+++ CaptchaSample/Controllers/PortalController.cs
@@ -0,0 +1,21 @@
+﻿using CaptchaSample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaptchaSample.Controllers
+{
+    public class PortalController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel loginViewModel)
+        {
+            return View();
+        }
+    }
+}
